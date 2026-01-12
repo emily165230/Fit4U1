d42d32c78ac8e6e71513e6842c3adb4f1c7a8a4e
emily165230 <emilyr28110@gmail.com>
2026-01-12 08:06:33 +0200

    progress 1.2

--- app/src/main/java/com/example/fit4u/HomeActivity.java
+++ app/src/main/java/com/example/fit4u/HomeActivity.java
@@ -1,12 +1,19 @@
 package com.example.fit4u;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+    private static final String TAG = "Emily and linoy";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.d(TAG,"onCreate");
+
     }
+
+
 }
